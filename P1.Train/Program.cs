241ed7aa76bf1e0d8875a00398d2a4801d0bd0e5
I-M-Marinov <lcfrrr@gmail.com>
2023-06-11 06:19:04 -0500

    Add project files.

---
+++ P1.Train/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P1.Train
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> wagonsList = Console.ReadLine().Split().Select(int.Parse).ToList();
+            int capacity = int.Parse(Console.ReadLine());
+
+            while (true)
+            {
+
+                string input = Console.ReadLine();
+
+                if (input == "end")
+                {
+                    break;
+                }
+
+                List<string> commands = input.Split().ToList();
+
+                if (commands[0] == "Add")
+                {
+                    wagonsList.Add(int.Parse(commands[1]));
+                }
+                else if (commands[0] != "Add")
+
+                {
+                    for (int i = 0; i < wagonsList.Count; i++)
+                    {
+                        if (int.Parse(commands[0]) + wagonsList[i] <= capacity)
+                        {
+                            wagonsList[i] += int.Parse(commands[0]);
+                            break;
+                        }
+                    }
+
+                }
+            }
+            Console.WriteLine(string.Join(" ", wagonsList));
+        }
+    }
+}
+      
+
+ 
