92b6fc331bd684280996a4c504b13b24aaa08c67
Ivan Marinov <134225981+I-M-Marinov@users.noreply.github.com>
2023-06-12 14:46:29 +0300

    Solution for P5. Bomb Numbers not ready yet
    Solution for P5. Bomb Numbers not ready yet

---
+++ P5.BombNumbers/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P5.BombNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> listOfNumbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+            List<string> bombNumberAndPower = Console.ReadLine().Split().ToList();
+
+            int bombNumber = int.Parse(bombNumberAndPower[0]);
+            int bombNumPower = int.Parse(bombNumberAndPower[1]);
+
+            for (int i = 0; i < listOfNumbers.Count; i++)
+            {
+                if (listOfNumbers[i] == bombNumber)
+                {
+                    for (int j = bombNumber; j > bombNumber - bombNumPower; j--)
+                    {
+                        listOfNumbers.Remove(listOfNumbers[j]);
+                    }
+                }
+            }
+
+            for (int i = 0; i < listOfNumbers.Count; i++)
+            {
+                if (listOfNumbers[i] == bombNumber) 
+                {
+                    for (int j = bombNumber; j < bombNumber + bombNumPower; j++)
+                    {
+                        listOfNumbers.Remove(listOfNumbers[j]);
+                    }
+                }
+            }
+            
+
+            for (int i = 0; i < listOfNumbers.Count; i++)
+            {
+                if (listOfNumbers[i] == bombNumber)
+                {
+                    listOfNumbers.Remove(bombNumber);
+                }
+            }
+            Console.WriteLine(listOfNumbers.Sum());    
+        }
+    }
+}
